095863179b8c9f2cfd05caa19f6a3242821edf94
Sonyahon <volkov030@gmail.com>
2020-02-04 02:11:14 +0300

    ic

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {Transport} from "@nestjs/common/enums/transport.enum";
+
+async function bootstrap() {
+	const app = await NestFactory.createMicroservice(AppModule, {
+		transport: Transport.TCP,
+		options: {
+			port: 3001
+		}
+	});
+	await app.listen(() => {
+		console.log('User service is listening');
+	});
+}
+
+bootstrap();
